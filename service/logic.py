e49ef78cfe8114b39afb8ce55244d5b76393e178
inbasu <just.me.juncy@gmail.com>
2023-08-06 01:10:07 +0300

    modified:   service/logic.py

--- service/logic.py
+++ service/logic.py
@@ -1,23 +1,27 @@
-from abc import ABC, abstractmethod
+from abc import ABC
 
 from database import crud
-from database.schemas import CreateMenu, Dish, Menu, UpdateMenu
+from database.schemas import BaseModel, CreateMenu, Menu, UpdateMenu
 
 
 class BaseService(ABC):
     database: crud.ModelRepository
 
-    async def create(self, create_menu: CreateMenu) -> Menu:
-        menu = await self.database.create(create_menu)
-        return menu
-
-    async def get(self, **kwargs) -> Menu | None:
-        item: Menu | None = await self.database.get(**kwargs)
+    async def create(self, create_menu: CreateMenu) -> BaseModel:
+        item = await self.database.create(create_menu)
         return item
 
-    @abstractmethod
-    async def update(self, update_model: UpdateMenu, **kwargs) -> Menu | None:
-        pass
+    async def get_all(self, **kwargs) -> list[BaseModel]:
+        all_items = await self.database.get_all(**kwargs)
+        return all_items
+
+    async def get(self, **kwargs) -> Menu | None:
+        item: BaseModel | None = await self.database.get(**kwargs)
+        return item
+
+    async def update(self, update_model: UpdateMenu, **kwargs) -> BaseModel | None:
+        item: BaseModel | None = await self.database.update(update_model, **kwargs)
+        return item
 
     async def delete(self, **kwargs) -> bool:
         return await self.database.delete()
@@ -27,36 +31,12 @@ class MenuService(BaseService):
     def __init__(self) -> None:
         self.database = crud.MenuRepository()
 
-    async def update(self, update_model: UpdateMenu, **kwargs) -> Menu | None:
-        item: Menu | None = await self.database.update(update_model, **kwargs)
-        return item
-
-    async def get_all(self) -> list[Menu]:
-        all_items: list[Menu] = await self.database.get_all()
-        return all_items
-
 
 class SubMenuService(BaseService):
     def __init__(self) -> None:
         self.database = crud.SubMenuRepository()
 
-    async def get_all(self, to_menu) -> list[Menu]:
-        all_items: list[Menu] = await self.database.get_all(to_menu=to_menu)
-        return all_items
-
-    async def update(self, update_model: UpdateMenu, **kwargs) -> Menu | None:
-        item: Menu | None = await self.database.update(update_model, **kwargs)
-        return item
-
 
 class DishService(BaseService):
     def __init__(self) -> None:
-        self.database = crud.SubMenuRepository()
-
-    async def get_all(self, to_submenu) -> list[Dish]:
-        all_items: list[Menu] = await self.database.get_all(to_submenu=to_submenu)
-        return all_items
-
-    async def update(self, update_model: UpdateMenu, **kwargs) -> Menu | None:
-        item: Menu | None = await self.database.update(update_model, **kwargs)
-        return item
+        self.database = crud.DishRepository()
